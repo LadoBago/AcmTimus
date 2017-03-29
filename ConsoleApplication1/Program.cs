59eb33dab3cc692c8b12c54c55b837a41d468c03
LadoBago <vl.bagoshvili@gmail.com>
2017-03-30 01:02:36 +0400

    c13

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        private static List<int>[] s, w;
         static void Main(string[] args)
         {
 #if !ONLINE_JUDGE
@@ -17,6 +18,15 @@ namespace ConsoleApplication1
 #endif
             int n = Convert.ToInt32(Console.ReadLine());
             bool[,] a = new bool[n, n];
+
+            s = new List<int>[n];
+            w = new List<int>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                s[i] = new List<int>();
+                w[i] = new List<int>();
+            }
 
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
@@ -29,18 +39,42 @@ namespace ConsoleApplication1
                 for (int j = 0; j < n; j++)
                 {
                     if (line[j] == '1')
-                        a[j, i] = true;
+                    {
+                        a[i, j] = true;
+                        s[i].Add(j);
+                        w[j].Add(i);
+                    }
                 }
             }
-
 
             int q = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < q; i++)
             {
                 int[] tokens = Console.ReadLine().Split(' ', '\t').Select(e => int.Parse(e)).ToArray();
-                Console.Write(!a[tokens[0], tokens[1]] ? "YES" : "NO");
+                int t1 = tokens[0] - 1;
+                int t2 = tokens[1] - 1;
+
+                if (IsRout(t1, t2))
+                {
+                    if (w[t1].Count == 0)
+                        Console.WriteLine("YES");
+                    else
+                        Console.WriteLine("NO");
+                }
+                else if (IsRout(t2, t1))
+                {
+                    if (w[t2].Count == 0)
+                        Console.WriteLine("YES");
+                    else
+                        Console.WriteLine("NO");
+                }
+                else
+                    Console.WriteLine("YES");
             }
+            /*
+             რუთები მოვძებნო, ანუ რომლისთვისაც არავის მოუგია და იმათი DFS ები დავიმახსოვრო.
+             */
 
 
 #if !ONLINE_JUDGE
@@ -49,6 +83,23 @@ namespace ConsoleApplication1
             sw.Flush();
             sw.Close();
 #endif
+        }
+
+        private static bool IsRout(int i, int j)
+        {
+            bool res = false;
+            foreach (int v in s[i])
+            {
+                if (v == j)
+                {
+                    res = true;
+                    break;
+                }
+                res = IsRout(v, j);
+                if (res)
+                    break;
+            }
+            return res;
         }
     }
 }
