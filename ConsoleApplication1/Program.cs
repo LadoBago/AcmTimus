1dbc92873d96462374f7d0a854fe53ec9ffdedfe
LadoBago <vl.bagoshvili@gmail.com>
2017-05-01 00:49:16 +0400

    c24

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace ConsoleApplication1
 {
@@ -15,32 +16,51 @@ namespace ConsoleApplication1
             Console.SetIn(sr);
             Console.SetOut(sw);
 #endif
-            int N = int.Parse(Console.ReadLine());
-            int E = int.Parse(Console.ReadLine());
-            int[] edges = new int[E * 3];
-            for (int i = 0; i < E; ++i)
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
             {
-                int[] tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(e => int.Parse(e)).ToArray();
-                Array.Copy(tokens, 0, edges, 3 * i, 3);
+                BigInteger res = rec(i);
+                Console.WriteLine(res.ToString());
+                Console.WriteLine(BigInteger.Remainder(res, BigInteger.Pow(new BigInteger(2), i)).ToString());
             }
-
-            GraphDrawing gd = new GraphDrawing();
-            int[] ret = gd.plot(N, edges);
-
-            Console.WriteLine(gd.Cnt);
-            Console.WriteLine(gd.maxK);
-            Console.WriteLine(ret.Length);
-            for (int i = 0; i < ret.Length; ++i)
-            {
-                Console.WriteLine(ret[i]);
-            }
-
 #if !ONLINE_JUDGE
             ;
             sr.Close();
             sw.Flush();
             sw.Close();
 #endif
+        }
+
+        private static BigInteger rec(int n)
+        {
+            switch (n)
+            {
+                case 0: return 1;
+                case 1: return 2;
+                case 2: return 12;
+                case 3: return 112;
+                case 4: return 112;
+                case 5: return 2112;
+                case 6: return 2112;
+                case 7: return 122112;
+                case 8: return 122112;
+            }
+
+            BigInteger prev = rec(n - 1);
+            BigInteger twos = BigInteger.Pow(2, n - 1);
+            BigInteger pr = BigInteger.Divide(prev, twos);
+            if (pr.IsEven)
+                return prev;
+
+            int len = prev.ToString().Length;
+            BigInteger tens = BigInteger.Pow(new BigInteger(10), len);
+            BigInteger m = BigInteger.Remainder(tens, twos);
+            BigInteger d = BigInteger.Divide(twos, m);
+            BigInteger r = rec((int)BigInteger.Log(d, 2D));
+
+            BigInteger res = BigInteger.Add(BigInteger.Multiply(r, tens), prev);
+
+            return res;
         }
     }
 }
